9bb19f54c164b021736215ef0c64e329c31665e5
Punit Kumar <punitchouchan00@gmail.com>
2025-12-24 10:20:55 +0530

    main screen complete

---
+++ app/src/main/java/com/example/learning/Screen.kt
@@ -0,0 +1,14 @@
+package com.example.learning
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Screen (){
+
+    Column {
+        Text(text = "Hlo")
+    }
+
+}
