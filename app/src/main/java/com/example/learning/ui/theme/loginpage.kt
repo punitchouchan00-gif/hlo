e17356b3ecd78bae131dccd828935433b7980e81
Punit Kumar <punitchouchan00@gmail.com>
2025-12-24 10:33:13 +0530

    loginscreen

---
+++ app/src/main/java/com/example/learning/ui/theme/loginpage.kt
@@ -0,0 +1,14 @@
+package com.example.learning.ui.theme
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SCreen (){
+
+    Text(text = "Hlo")
+
+}
+
+
+
